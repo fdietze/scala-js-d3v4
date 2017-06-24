946b8329d8bcb8f94cdad60d6deb20786882c433
Felix Dietze <felix@beerleader.net>
2017-06-24 17:26:52 +0200

    polygon facades

--- src/main/scala/Polygon.scala
+++ src/main/scala/Polygon.scala
@@ -1,12 +1,19 @@
 package org.scalajs.d3v4
 
 import scalajs.js
-import scalajs.js.{undefined, `|`}
+import scalajs.js.{ undefined, `|` }
 import scala.scalajs.js.annotation._
 
 // https://github.com/d3/d3-polygon
 @JSImport("d3-polygon", JSImport.Namespace)
 @js.native
 object d3polygon extends js.Object {
-  def polygonHull(points: js.Array[js.Array[Double]]): js.Array[js.Array[Double]] | Null = js.native
+  type Point = js.Tuple2[Double, Double]
+  type Polygon = js.Array[Point]
+
+  def polygonArea(polygon: Polygon): Double = js.native
+  def polygonCentroid(polygon: Polygon): Point = js.native
+  def polygonHull(points: Polygon): Polygon | Null = js.native
+  def polygonContains(polygon: Polygon, point: Point): Boolean = js.native
+  def polygonLength(polygon: Polygon): Boolean = js.native
 }
--- src/main/scala/Shape.scala
+++ src/main/scala/Shape.scala
@@ -1,27 +1,27 @@
 package org.scalajs.d3v4
 
 import scalajs.js
-import scalajs.js.{undefined, `|`}
+import scalajs.js.{ undefined, `|` }
 import scala.scalajs.js.annotation._
 
 // https://github.com/d3/d3-shape
 @JSImport("d3-shape", JSImport.Namespace)
 @js.native
 object d3shape extends js.Object {
-  def pie():PieGenerator = js.native
-  def arc():ArcGenerator = js.native
+  def pie(): PieGenerator = js.native
+  def arc(): ArcGenerator = js.native
   def line(): LineGenerator = js.native
   def curveCatmullRomClosed: CurveCatmullRomFactory = js.native
 }
 
 @js.native
 trait BaseLineGenerator[G <: BaseLineGenerator[G]] extends js.Object {
-  def curve(curve:CurveFactory):G = js.native
+  def curve(curve: CurveFactory): G = js.native
 }
 
 @js.native
 trait LineGenerator extends BaseLineGenerator[LineGenerator] {
-  def apply(data:js.Array[js.Array[Double]]):String = js.native
+  def apply(data: js.Array[js.Tuple2[Double, Double]]): String = js.native
 }
 
 @js.native
@@ -30,25 +30,24 @@ trait CurveFactory extends js.Object {
 
 @js.native
 trait CurveCatmullRomFactory extends CurveFactory {
-  def alpha(alpha:Double):CurveCatmullRomFactory = js.native
+  def alpha(alpha: Double): CurveCatmullRomFactory = js.native
 }
-
 
 @js.native
 trait PieGenerator extends js.Object {
-  def value(value:Double):PieGenerator = js.native
-  def padAngle(angle:Double):PieGenerator = js.native
+  def value(value: Double): PieGenerator = js.native
+  def padAngle(angle: Double): PieGenerator = js.native
   def apply[Datum](data: js.Array[Datum]): js.Array[PieArcDatum[Datum]] = js.native
 }
 
 @js.native
 trait PieArcDatum[Datum] extends ArcDatum {
-def data: Datum = js.native
-def value: Double = js.native
-def index: Int = js.native
-def startAngle: Double = js.native
-def endAngle: Double = js.native
-def padAngle: Double = js.native
+  def data: Datum = js.native
+  def value: Double = js.native
+  def index: Int = js.native
+  def startAngle: Double = js.native
+  def endAngle: Double = js.native
+  def padAngle: Double = js.native
 }
 
 @js.native
@@ -56,18 +55,18 @@ trait ArcDatum extends js.Object
 
 @js.native
 trait BaseArcGenerator[G <: BaseArcGenerator[G]] extends js.Object {
-  def innerRadius(radius:Double):G = js.native
-  def outerRadius(radius:Double):G = js.native
-  def cornerRadius(radius:Double):G = js.native
+  def innerRadius(radius: Double): G = js.native
+  def outerRadius(radius: Double): G = js.native
+  def cornerRadius(radius: Double): G = js.native
 }
 
 @js.native
 trait ArcGenerator extends BaseArcGenerator[ArcGenerator] {
-  def apply[T <: ArcDatum](arguments:T):String = js.native
-  def centroid[T <: ArcDatum](arguments:T):js.Array[Double] = js.native
+  def apply[T <: ArcDatum](arguments: T): String = js.native
+  def centroid[T <: ArcDatum](arguments: T): js.Tuple2[Double, Double] = js.native
 }
 
 @js.native
 trait ArcGeneratorWithContext extends BaseArcGenerator[ArcGeneratorWithContext] {
-  def apply[T <: ArcDatum](arguments:T):Unit = js.native
+  def apply[T <: ArcDatum](arguments: T): Unit = js.native
 }
