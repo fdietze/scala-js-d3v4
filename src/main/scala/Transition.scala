624ac0bbe386736a8608f5d899100b13a871950e
Michael Ahlers <michael@ahlers.consulting>
2021-01-19 09:52:20 -0500

    Expand API coverage

--- src/main/scala/Transition.scala
+++ src/main/scala/Transition.scala
@@ -8,20 +8,38 @@ import scala.scalajs.js.annotation._
 @js.native
 object d3transition extends js.Object {
 
+  /** @see [[https://github.com/d3/d3-transition#transition_delay]] */
+  def delay(value: Double): Transition[dom.EventTarget] = js.native
+
+  /** @see [[https://github.com/d3/d3-transition#transition_duration]] */
+  def duration(value: Double): Transition[dom.EventTarget] = js.native
+
   /** @see [[https://github.com/d3/d3-transition#transition]] */
   def transition(): Transition[dom.EventTarget] = js.native
 
   /** @see [[https://github.com/d3/d3-transition#transition]] */
   def transition(name: String): Transition[dom.EventTarget] = js.native
 
-  /** @see [[https://github.com/d3/d3-transition#transition_duration]] */
-  def duration(value: Double): Transition[dom.EventTarget] = js.native
-
   @js.native
   trait Transition[Datum] extends js.Object {
 
+    /** @see [[https://github.com/d3/d3-transition#transition_delay]] */
+    def delay(value: Double): Transition[Datum] = js.native
+
     /** @see [[https://github.com/d3/d3-transition#transition_duration]] */
     def duration(value: Double): Transition[Datum] = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_empty]] */
+    def empty():Boolean = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_nodes]] */
+    def nodes(): js.Array[dom.EventTarget] = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_node]] */
+    def node(): dom.EventTarget = js.native
+
+    /** @see [[https://github.com/d3/d3-transition#transition_size]] */
+    def size(): Int = js.native
 
   }
 
