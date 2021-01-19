1b190f94434975068f1ca369b637e518c891bb94
Michael Ahlers <michael@ahlers.consulting>
2021-01-19 09:54:39 -0500

    Code documentation

--- src/main/scala/Selection.scala
+++ src/main/scala/Selection.scala
@@ -106,7 +106,11 @@ object d3selection extends js.Object {
     def select[SelData](selector: String): Selection[SelData] = js.native
     def selectAll[SelData](selector: String): Selection[SelData] = js.native
     def node(): dom.EventTarget = js.native
+
+    /** @see [[d3transition]] */
     def transition(): Transition[Datum] = js.native
+
+    /** @see [[d3transition]] */
     def transition(name: String): Transition[Datum] = js.native
   }
 
