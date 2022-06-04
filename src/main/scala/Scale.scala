f4569384ba39b01abd191c5f214ff723b9f18982
Brent Haines <thebrenthaines@yahoo.com>
2022-06-04 10:30:44 -0700

    Added Band scale, corrected Ordinal scale, improved support for Axis and Array (#149)
    Co-authored-by: Brent Haines <brent@aifoundation.com>

--- src/main/scala/Scale.scala
+++ src/main/scala/Scale.scala
@@ -12,7 +12,8 @@ object d3scale extends js.Object {
   def scaleLinear(): LinearScale                           = js.native
   def scaleLog(): LogScale                                 = js.native
   def scaleTime(): TimeScale                               = js.native
-  def scaleOrdinal(values: js.Array[String]): OrdinalScale = js.native
+  def scaleBand[T,R](): BandScale[T,R]                     = js.native
+  def scaleOrdinal[T,R](values: js.Array[String]): OrdinalScale[T,R] = js.native
   def schemeCategory10: js.Array[String]                   = js.native
   def schemeCategory20: js.Array[String]                   = js.native
   def schemeCategory20b: js.Array[String]                  = js.native
@@ -23,10 +24,11 @@ object d3scale extends js.Object {
 
   @js.native
   trait ContinuousScale[S <: ContinuousScale[S]] extends Scale {
-    def apply(value: Double): Double        = js.native
-    def invert(value: Double): Double       = js.native
-    def domain(domain: js.Array[Double]): S = js.native
-    def range(range: js.Array[Double]): S   = js.native
+    def apply(value: Double): Double            = js.native
+    def invert(value: Double): Double           = js.native
+    def domain(domain: js.Array[Double]): S     = js.native
+    def range(range: js.Array[Double]): S       = js.native
+    def rangeRound(range: js.Array[Double]): S  = js.native
   }
 
   @js.native
@@ -44,8 +46,19 @@ object d3scale extends js.Object {
   }
 
   @js.native
-  trait OrdinalScale extends js.Object {
-    def domain(values: js.Array[String]): this.type = js.native
-    def apply(string: String): String               = js.native
+  trait OrdinalScale[T,R] extends Scale {
+    def domain(values: js.Array[T]): this.type    = js.native
+    def domain(): js.Array[T]                     = js.native
+    def range(): this.type                        = js.native
+    def range(range: js.Array[R]): this.type      = js.native
+    def apply(string: String): js.Any             = js.native
+  }
+
+  @js.native
+  trait BandScale[T,R] extends OrdinalScale[T,R] {
+    def apply(value: T): js.Any                           = js.native
+    def bandwidth(): Double                               = js.native
+    def rangeRound(range: js.Array[R]): this.type         = js.native
+    def padding(padding: Double): this.type               = js.native
   }
 }
