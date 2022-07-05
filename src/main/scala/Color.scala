f08bd2d251795a2a3c8852c1cd8074dfc457a357
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-07-06 01:13:54 +0200

    Update scalafmt-core to 3.5.8 (#155)
    * Update scalafmt-core to 3.5.8
    
    * Reformat with scalafmt 3.5.8
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/main/scala/Color.scala
+++ src/main/scala/Color.scala
@@ -18,10 +18,10 @@ object d3color extends js.Object {
   def hcl(specifier: String): Hcl                                = js.native
   def hcl(color: Color): Hcl                                     = js.native
 
-  def rgb(r: Int, g: Int, b: Int): Rgb                           = js.native
-  def rgb(r: Int, g: Int, b: Int, opacity: Int): Rgb             = js.native
-  def rgb(specifier: String): Rgb                                = js.native
-  def rgb(color: Color): Rgb                                     = js.native
+  def rgb(r: Int, g: Int, b: Int): Rgb               = js.native
+  def rgb(r: Int, g: Int, b: Int, opacity: Int): Rgb = js.native
+  def rgb(specifier: String): Rgb                    = js.native
+  def rgb(color: Color): Rgb                         = js.native
 
   @js.native
   trait Rgb extends Color {
